777ffdac4e2105db85b753e0a843e05bfe53df1a
itsmagical <itsmagical@163.com>
2021-05-28 11:30:35 +0800

    '修改图片声明和引用方式'

---
+++ lib/images/file_gallery_images.dart
@@ -0,0 +1,27 @@
+class VideoPlayerImages {
+
+  static const String video_player_start = 'packages/file_gallery/images/video_player_start.png';
+
+  static const String video_player_pause = 'packages/file_gallery/images/video_player_pause.png';
+
+  static const String status_error = 'packages/file_gallery/images/status_error.png';
+
+  /// word文档
+  static const String file_type_word = 'packages/file_gallery/images/file_type_word.png';
+
+  /// excel文档
+  static const String file_type_excel = 'packages/file_gallery/images/file_type_excel.png';
+
+  /// ppt文档
+  static const String file_type_ppt = 'packages/file_gallery/images/file_type_ppt.png';
+
+  /// pdf文档
+  static const String file_type_pdf = 'packages/file_gallery/images/file_type_pdf.png';
+
+  /// txt
+  static const String file_type_txt = 'packages/file_gallery/images/file_type_txt.png';
+
+  /// 未知文件类型
+  static const String file_type_unknown = 'packages/file_gallery/images/file_type_unknown.png';
+
+}
